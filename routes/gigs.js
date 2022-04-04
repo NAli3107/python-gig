95095e7651c8df292fd48842840e13b657b915b6
Andrea Mac <aleatrengrouse@gmail.com>
2022-04-04 22:07:37 +0100

    created db, routes, corrected file name gigs.js to Gig.js, moved code from gigs.js to routes/gigs.js,created public folder

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,18 +2,44 @@
 //Gig routes
 //app.use("/gigs", require("./routes/gigs"));
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../config/database');
-const Gig = require('../models/Gig');
+const db = require("../config/database");
+const Gig = require("../models/Gig");
 
-router.get('/', (req, res) =>
- Gig.findAll()
- .then(gigs => {
-  console.log(gigs);
-  res.sendStatus(200);
-})
- .catch(err => console.log(err)));
+//Get gi list
+router.get("/", (req, res) =>
+  Gig.findAll()
+    .then((gigs) => {
+      console.log(gigs);
+      res.sendStatus(200);
+    })
+    .catch((err) => console.log(err))
+);
+
+// Add a gig
+router.get("/add", (req, res) => {
+  const data = {
+    title: "React developer",
+    technologies: "react, javascript, html, css",
+    budget: "£3000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non gravida velit. Vivamus non tortor imperdiet, ultrices libero sed, vulputate urna. Aliquam commodo porta mauris interdum condimentum. Cras maximus volutpat mi, eu maximus sapien egestas aliquet.",
+    contact_email: "user1@gmail.com",
+  };
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  //Insert into table
+  Gig.create({
+    title,
+    technologies,
+    description,
+    budget,
+    contact_email,
+  })
+    .then((gig) => res.redirect("/gigs"))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
-
