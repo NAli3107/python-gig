fa03c664ce5358b717f8f98eb41195d820655654
Corey96 <0coreyy@gmail.com>
2022-04-04 16:20:44 +0100

    added gigs.handlebars html template and set static folder app.js

--- app.js
+++ app.js
@@ -17,3 +17,7 @@
 //Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.send('INDEX'));
