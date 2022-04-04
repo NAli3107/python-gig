443dc0429badf098ad1d72a7bc79b3f557961685
Corey96 <0coreyy@gmail.com>
2022-04-04 17:05:35 +0100

    merged app.js files

--- routes/gigs.js
+++ routes/gigs.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+// Get gig list
 router.get('/', (req, res) =>
  Gig.findAll()
  .then(gigs => {
@@ -16,7 +17,30 @@ router.get('/', (req, res) =>
 })
  .catch(err => console.log(err)));
 
- // Add a gig - Andrea
+// Add a gig
+router.get("/add", (req, res) => {
+  const data = {
+    title: "React developer",
+    technologies: "react, javascript, html, css",
+    budget: "£3000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non gravida velit. Vivamus non tortor imperdiet, ultrices libero sed, vulputate urna. Aliquam commodo porta mauris interdum condimentum. Cras maximus volutpat mi, eu maximus sapien egestas aliquet.",
+    contact_email: "user1@gmail.com",
+  };
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  // Insert into table
+   Gig.create({
+    title,
+    technologies,
+    description,
+    budget,
+    contact_email,
+  })
+    .then((gig) => res.redirect("/gigs"))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
 
