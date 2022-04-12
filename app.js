c7a8144b2a1c4de071e366794e156c9792f35a61
Andrea Mac <aleatrengrouse@gmail.com>
2022-04-12 15:43:48 +0100

    fixed a bug with dispaying all gigs in habndlebars

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require("./controllers");
-
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 //Database
+
 const db = require("./config/database");
 // const { Sequelize } = require("sequelize/types");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Handlebars
-app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+//Handlebars (non Handlebars bug requires InsecurePrototypeAccess to work)
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
 
 // Body Parser
@@ -27,8 +37,6 @@ app.use(routes);
 
 //test DB
 // db.authenticate().then(() => console.log("Database connected..."));
-
-
 
 db.sync({ force: false }).then(() => {
   app.listen(PORT, console.log(`Server Started on port ${PORT}`));
